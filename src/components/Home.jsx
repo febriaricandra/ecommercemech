b8f34e5cd04d6746d40437b0501da3a43f398898
febriaricandra <febriaricandraproduction@gmail.com>
2022-05-07 08:21:12 +0700

    add products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Products from './Products'
 
 function Home() {
     return (
@@ -7,11 +8,12 @@ function Home() {
                 <img src="../assets/bg.jpg" className="card-img" alt="background" height="550px" />
                     <div className="card-img-overlay d-flex flex-column justify-content-center">
                         <div className="container">
-                        <h5 className="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
+                        <h5 className="card-title display-3 fw-bolder mb-0 h5-rem">New Season Arrivals</h5>
                         <p className="card-text lead fs-2">Check Out All The Trends</p>
                         </div>
                     </div>
             </div>
+            <Products />
         </div>
     )
 }
