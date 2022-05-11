71db6057f58deac479992ca5e28a59fa9e35beb8
febriaricandra <febriaricandraproduction@gmail.com>
2022-05-11 15:24:04 +0700

    add redux action store reducers

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Skeleton from 'react-loading-skeleton';
-
+import {NavLink} from 'react-router-dom'
 function Products() {
   const [data, setData] = useState([])
   const [filter, setFilter] = useState(data)
@@ -68,7 +68,7 @@ function Products() {
                   <div className="card-body">
                     <h5 className="card-title mb-0">{product.title.substring(0, 12)}...</h5>
                     <p className="card-text lead fw-bold">${product.price}</p>
-                    <a href="/" className="btn btn-outline-dark">Buy Now</a>
+                    <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
                   </div>
                 </div>
               </div>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
+import { Provider } from 'react-redux';
+import store from './redux/reducers/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
